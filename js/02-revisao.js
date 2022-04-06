93db151b771d8cab819347f01750106dae7efdc8
HttpCaldeira <gui-caldeira2011@hotmail.com>
2022-04-06 17:02:34 -0300

    Adição pág. revisão js

---
+++ js/02-revisao.js
@@ -0,0 +1,9 @@
+console.log("Revisão do JS Senac!"); /* comando para mostrar no console do navegador */
+
+let camiseta = "Suco de Laranja";
+let peso = "100g"
+
+const planeta = "Terra"
+
+
+console.log(`Moro no planeta ${planeta}`);
