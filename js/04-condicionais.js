a87e09cb3ba0afcae6d18cf81fc7bdcf6265ab5a
HttpCaldeira <gui-caldeira2011@hotmail.com>
2022-04-12 14:20:07 -0300

    Estrutura de Loops

--- js/04-condicionais.js
+++ js/04-condicionais.js
@@ -64,3 +64,5 @@ if( media < 3){
 
 console.log(desempenho);
 
+
+
---
+++ js/05-loops.js
@@ -0,0 +1,19 @@
+/* Estruturas Tradicionais
+(Tem em todas as linguagens)
+
+while -> Enquanto
+do/while -> faça/enquanto
+for -> para 
+
+*/
+
+// Exemplo Loop while (enquanto)
+
+console.log("while");
+
+// Variavel de controle do loop i
+let contador = 1;
+// Enquanto o CONTADOR for meno ou igual a 10
+while(contador <= 10){
+    console.log(`Valor de contador`);
+}
