6de3afe8e37bbd4f89e2acdf4a453137e8c20875
HttpCaldeira <gui-caldeira2011@hotmail.com>
2022-04-13 14:03:16 -0300

    Arrow Function
    Arrow Function
    Retorno IMPLICITO

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -24,11 +24,19 @@ linha(); /* Chamando função Sempre Retorna algum resultado */
 
 
                   //Parametros ou Argumentos 
-function soma( valor1, valor2 ){
+/* function soma( valor1, valor2 ){
     let total = valor1 + valor2;
     return total;
-} 
+}  */
 
+// Versão 2: sintaxe Arrow Function (Função Seta/Flecha) Asssociada a uma constanto um codigo que nao vai receber alteração
+
+const soma = ( valor1, valor2 ) => {
+    let total = valor1 + valor2;
+    return total
+};
+
+const soma1 = (valor1, valor2) => valor1 + valor2;
 
 console.log( soma(10, 5) );
 
@@ -38,3 +46,37 @@ console.log(resultado);
 if(resultado < 1000){
     console.log("Abaixo da Meta");
 }
+
+linha();
+
+// Versão 1 Tradicional
+/* function dobra( valor ){
+    let resultado = valor *2
+    return resultado
+}
+ */
+/* 
+function dobra(valor){
+    return valor *2
+}
+ */
+
+
+// Versaõ 2: Arrow Function
+// const dobra = (valor) => {
+//     let resultado = valor *2;
+//     return resultado
+// };
+
+
+// const dobra = (valor) =>{
+//     return  valor *2
+// }
+
+// retorno Implicito
+// Obs... como só temos um parâmetro (valor), pode tirar os ()
+const dobra = valor => valor * 2;
+
+
+console.log( dobra ( 10 )  );
+console.log( dobra ( 150 ) );
